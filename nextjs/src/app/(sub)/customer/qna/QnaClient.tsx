4321e28ed54782156aa03bfab0692c39d94abfcc
kookOh <skuk27@gmail.com>
2025-08-23 04:42:50 +0900

    qna 데이터 수정

--- nextjs/src/app/(sub)/customer/qna/QnaClient.tsx
+++ nextjs/src/app/(sub)/customer/qna/QnaClient.tsx
@@ -26,7 +26,7 @@ export default function QnaClient() {
         const response = await fetch('/api/faqs')
         if (response.ok) {
           const data = await response.json()
-          setFaqs(data.faqs)
+          setFaqs(data.posts)
         } else {
           setError('FAQ를 불러오는데 실패했습니다.')
         }
